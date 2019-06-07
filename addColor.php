65e90d5149b968ee631646920c729cb1af7a88a2
textndot <48188102+textndot@users.noreply.github.com>
2019-06-07 15:49:47 +0900

    Add files via upload

--- addColor.php
+++ addColor.php
@@ -1,11 +1,18 @@
 <? php
-if(isset($_POST['opacity'])){
-    $opacity = $_POST['opacity'];
-    $con = new mysqli($db_hostname, $db_username, $db_password, $db_databasename);
-    $stmt = $con->prepare("UPDATE `opacity_tbl` SET `opacity` = ?");
-    $stmt->bind_param("s", $color);
-    $stmt->execute();
-    $stmt->close();
-}
+$data = json_decode($_GET['data']);
+header("Content-Type: application/json");
+$opacity = "";
 
-?>
+if($method == "GET")
+{ // 1. 자바스크립트 객체 또는 serialize() 로 전달
+  $opacity = $_GET['opacity'];
+  echo(json_encode(array("mode" => $_REQUEST['mode'], "opacity" => $opacity)));}
+
+else if($method == "POST")
+  { // 1. 자바스크립트 객체 또는 serialize() 로 전달
+    $data = json_decode($_POST['data']);
+    echo(json_encode(array("mode" => $_REQUEST['mode'], "opacity" => $data->opacity))); }
+
+
+
+    ?>
